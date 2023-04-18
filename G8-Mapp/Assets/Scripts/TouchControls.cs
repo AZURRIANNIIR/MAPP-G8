003c35bd8ad0080ce6689beb8412f2c9f5c69a9e
MarioGuy3 <jonathan.tallgren@gmail.com>
2023-04-18 10:35:18 +0200

    Updatering av TouchControls.cs
    * Skriptet lägger nu till en TrailRenderer om det saknas
    
    * En konstant som representerar värdet för vänster musknapp har lagts till
    
    * Lagt till en Vector2 som fungerar som startPosition för ormen
    
    * Trailen för ormen återställs nu om musknappen släpps upp. Ormen återstälsl också till startpositionen via...
    
    *En funktion som har lagts till

--- G8-Mapp/Assets/Scripts/TouchControls.cs
+++ G8-Mapp/Assets/Scripts/TouchControls.cs
@@ -8,22 +8,30 @@ using UnityEngine.UI;
 using Touch = UnityEngine.Touch;
 //using UnityEngine.InputSystem;
 
+[RequireComponent(typeof(TrailRenderer))]
+
 public class TouchControls : MonoBehaviour
 {
+    private const int LMB_NUMBER = 0;
 
     [SerializeField] private float movementLength;
     [SerializeField] private GameObject snake;
     [SerializeField] private LayerMask mask;
+    [SerializeField] private TrailRenderer snakeTrailRenderer;
     private bool snakeCaught = false;
     [SerializeField] private float lerpFactor = 1000f;
 
     private float width;
     private float height;
 
+    private Vector2 startPosition;
+
     private void Awake()
     {
-        width = (float)Screen.width;
-        height = (float)Screen.height;
+        snakeTrailRenderer = GetComponent<TrailRenderer>();
+        width = Screen.width;
+        height = Screen.height;
+        startPosition = transform.position;
     }
 
     // Update is called once per frame
@@ -32,21 +40,22 @@ public class TouchControls : MonoBehaviour
         Vector3 mousePos = Input.mousePosition;
         mousePos = Camera.main.ScreenToWorldPoint(mousePos);
 
-        if (Input.GetMouseButton(0) && RaycastIsHitting(mousePos))
+        if (Input.GetMouseButton(LMB_NUMBER) && RaycastIsHitting(mousePos))
         {
             snakeCaught = true;
             print("mus ner");
         }
 
-        if (Input.GetMouseButtonUp(0))
+        if (Input.GetMouseButtonUp(LMB_NUMBER) && snakeCaught)
         {
             snakeCaught = false;
+            ResetSnakeToStart();
+            snakeTrailRenderer.Clear();
         }
 
         if (snakeCaught)
         {
             transform.position = Vector2.Lerp(transform.position, mousePos, lerpFactor);
-
         }
     }
 
@@ -59,6 +68,11 @@ public class TouchControls : MonoBehaviour
             return true;
         }
         return false;
+    }
+
+    private void ResetSnakeToStart()
+    {
+        transform.position = startPosition;
     }
 
 }
