30f81d3c91f9bbf60c06aa3898aeb0dda433d532
Fangelsnoof <elsaa.petersson@gmail.com>
2023-04-20 10:15:35 +0200

    Börjat på bro-mekanik

---
+++ G8-Mapp/Assets/Scripts/BridgeTile.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BridgeTile : MonoBehaviour
+{
+
+    public bool crossedOnce = false;
+    public bool taken = false;
+    [SerializeField] private ColliderScript tileCollider;
+    [SerializeField] private GameController gameController;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Snake") && crossedOnce)
+        {
+            taken = true;
+            print("ny plats");
+            //tileCollider.TakeTile();
+            gameController.tileTaken();
+        }
+
+        crossedOnce = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Snake"))
+        {
+            print(taken);
+        }
+    }
+
+    public void SetTakenStatus(bool state)
+    {
+        taken = state;
+    }
+}
+
