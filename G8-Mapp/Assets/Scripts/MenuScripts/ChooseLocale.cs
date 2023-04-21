75aab3539694120f93970dcabb0245c30ab5865b
AZURRIANNIIR <113354208+AZURRIANNIIR@users.noreply.github.com>
2023-04-21 11:18:28 +0200

    Små ändringar

--- G8-Mapp/Assets/Scripts/MenuScripts/ChooseLocale.cs
+++ G8-Mapp/Assets/Scripts/MenuScripts/ChooseLocale.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class ChooseLocale : MonoBehaviour
 {
+    //Definerar våra två knappar
+  // public Button spanishButton
+  // public Button englishButton
+
+
     // Start is called before the first frame update
     void Start()
     {
