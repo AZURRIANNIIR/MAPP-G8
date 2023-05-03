2ea115c3d8223c409e01b29f7692003d9311b716
Emomoa <113351340+Emomoa@users.noreply.github.com>
2023-05-03 13:15:38 +0200

    bläddra mellan help paneler

--- G8-Mapp/Assets/Scripts/HelpPanelScript.cs
+++ G8-Mapp/Assets/Scripts/HelpPanelScript.cs
@@ -5,10 +5,10 @@ using UnityEngine.UI;
 
 public class HelpPanelScript : MonoBehaviour
 {
-    [SerializeField] private GameObject panelOne;
-    [SerializeField] private GameObject panelTwo;
-    [SerializeField] private GameObject panelThree;
-    static int pressAmount;
+    [SerializeField] private Image panelOne;
+    [SerializeField] private Image panelTwo;
+    [SerializeField] private Image panelThree;
+    [SerializeField] static int pressAmount;
     [SerializeField] private int panelChange;
     [SerializeField] private Button button;
 
@@ -17,15 +17,70 @@ public class HelpPanelScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        button.onClick.AddListener(TaskOnClick);
+        ChangePanel();
     }
+
+    void TaskOnClick()
+    {
+        pressAmount += panelChange;
+        ChangePanel();
+        checkButtonEnabled();
+    }
+
 
     // Update is called once per frame
     void Update()
     {
-        button.onClick.Invoke();
+        if (panelChange == 1)
+        {
+            if (pressAmount == 2)
+            {
+                button.enabled = false;
+            }
+            else
+            {
+                button.enabled = true;
+            }
+        }
+
+        if (panelChange == -1)
+        {
+            if (pressAmount == 0)
+            {
+                button.enabled = false;
+            }
+            else
+            {
+                button.enabled = true;
+            }
+        }
     }
 
+    private void ChangePanel()
+    {
+        if (pressAmount == 0)
+        {
+            panelOne.enabled = true;
+            panelTwo.enabled = false;
+            panelThree.enabled = false;
+        } else if (pressAmount == 1)
+        {
+            panelOne.enabled = false;
+            panelTwo.enabled = true;
+            panelThree.enabled = false;
+        } else if (pressAmount == 2)
+        {
+            panelOne.enabled = false;
+            panelTwo.enabled = false;
+            panelThree.enabled = true;
+        }
+    }
+
+    private void checkButtonEnabled()
+    {
+
+    }
 
 
 }
