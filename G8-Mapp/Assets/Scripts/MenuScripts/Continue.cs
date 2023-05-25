542d864fdf186e8b4369bfc426c1fbed20b1c6b7
Hanna Arrhenius <113351241+hannaarrhenius@users.noreply.github.com>
2023-05-25 11:50:34 +0200

    Continue Script
    har löst continue grejen helt nu (hoppas jag) !!

--- G8-Mapp/Assets/Scripts/MenuScripts/Continue.cs
+++ G8-Mapp/Assets/Scripts/MenuScripts/Continue.cs
@@ -16,22 +16,31 @@ public class Continue : MonoBehaviour
     {
         //korutinmetod som gör att scenen laddas med 1 sekunds delay(för att vi vill ha knappljudet vid tryckning) Efter fördröjningen kan scenen laddas in med nästa rad
         yield return new WaitForSeconds(1);
-        SceneManager.LoadScene(PlayerPrefs.GetInt("SavedScene"));
+        int savedScene = PlayerPrefs.GetInt("SavedScene");
+        if(savedScene > 0)
+        {
+            SceneManager.LoadScene(savedScene);
+        }
+        else
+        {
+            SceneManager.LoadScene("Level 1");
+        }
+
+       // SceneManager.LoadScene(PlayerPrefs.GetInt("SavedScene"));
         Debug.Log(PlayerPrefs.GetInt("SavedScene"));
 
     }
     private void OnApplicationFocus(bool focus)
     {
         //Om spelaren går ut ur spelet osv sparas det nuvarande buildIndex med playerPrefs, vilket gör att nuvarande scen sparas var spelaren lämnade det
-        if (focus)
-        { }
-        else
+        if (!focus)
         {
             PlayerPrefs.SetInt("SavedScene",SceneManager.GetActiveScene().buildIndex);
             Debug.Log(PlayerPrefs.GetInt("SavedScene"));
 
         }
     }
+ 
 
     public void SaveScene()
     {
